a454c63479e67cb4bcf93d5aeb1173575ba47510
Roni Kettunen <ronikettunen96@gmail.com>
2018-12-20 09:51:23 +0200

    Update recommendation.py

--- src/recommendation.py
+++ src/recommendation.py
@@ -5,7 +5,7 @@ from create_XY import create_user, get_restaurantname
 
 def recommend(user):
     user = user.reshape(1, -1)
-    clf = joblib.load('model/random_forestV2.joblib')
+    clf = joblib.load('model/random_forestV3.joblib')
     return clf.predict(user)[0]
 
 user = create_user('GIVE HERE AN EXAMPLE EMAIL')[0]
